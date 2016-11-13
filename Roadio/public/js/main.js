1a78014628b121d9058ff5852a037d5197bf5bb4
Abhishek Mangla <amangla@berkeley.edu>
2016-11-13 03:14:13 -0800

    finished POI rotation

--- Roadio/public/js/main.js
+++ Roadio/public/js/main.js
@@ -14,15 +14,36 @@ $(window).on('load',function () {
     potentialPOIsFreeDays(A,B,3,keyword,function(POIs,freeDays){
         $("#photo-container").find(".front").attr("src", POIs[0].image_url);
         $("#photo-container").find(".back").attr("src", POIs[1].image_url);
+        $(".title").text(POIs[0].name);
+        $(".review").text(POIs[0].snippet_text);
         originalPOIs = POIs;
+
         go = function(){
-            desiredPOIs.push(POIs[iterationIndex]);
-            console.log(iterationIndex);
-            iterationIndex++;
+            
+            if (iteratorIndex >= freeDays) {
+              window.location.href = "rotationPOIpicker.jade"
+            }
+            else {
+              desiredPOIs.push(POIs[iterationIndex]);
+              console.log(iterationIndex);
+              iterationIndex++;
+              $(".title").text(POIs[iterationIndex].name);
+              $(".review").text(POIs[iterationIndex].snippet_text);
+            }
+            
         };
         not_go = function(){
-            console.log(iterationIndex);
-            iterationIndex++;
+            
+            if (iteratorIndex >= freeDays) {
+
+            }
+            else {
+              console.log(iterationIndex);
+              iterationIndex++;
+              $(".title").text(POIs[iterationIndex].name);
+              $(".review").text(POIs[iterationIndex].snippet_text);
+            }
+            
         };
     });
 });
@@ -31,10 +52,10 @@ $(document).ready(function() {
   $("#going").click(function(){
       if ($("#photo-container").find(".front").hasClass("hidden")) {
           $("#photo-container").find(".front").animate({opacity : 1}, {duration: 200, queue: false});
-          $("#photo-container").find(".back").attr("src", originalPOIs[iterationIndex + 1].image_url);
+          $("#photo-container").find(".back").attr("src", originalPOIs[iterationIndex + 2].image_url);
       } else {
           $("#photo-container").find(".front").animate({opacity : 0}, {duration: 200, queue: false});
-          $("#photo-container").find(".front").attr("src", originalPOIs[iterationIndex + 1].image_url);
+          $("#photo-container").find(".front").attr("src", originalPOIs[iterationIndex + 2].image_url);
       }
       $("#photo-container").find(".front").toggleClass("hidden");
       go();
@@ -43,10 +64,10 @@ $(document).ready(function() {
   $("#not-going").click(function(){
       if ($("#photo-container").find(".front").hasClass("hidden")) {
           $("#photo-container").find(".front").animate({opacity : 1}, {duration: 200, queue: false});
-          $("#photo-container").find(".back").attr("src", originalPOIs[iterationIndex + 1].image_url);
+          $("#photo-container").find(".back").attr("src", originalPOIs[iterationIndex + 2].image_url);
       } else {
           $("#photo-container").find(".front").animate({opacity : 0}, {duration: 200, queue: false});
-          $("#photo-container").find(".front").attr("src", originalPOIs[iterationIndex + 1].image_url);
+          $("#photo-container").find(".front").attr("src", originalPOIs[iterationIndex + 2].image_url);
       }
       $("#photo-container").find(".front").toggleClass("hidden");
       not_go();
