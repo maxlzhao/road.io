5c52c534765cd97471d90e608ab45013ee5744f5
kchua <kurtlandchua@yahoo.com>
2016-11-12 18:42:43 -0800

    Change default params for POI

--- Roadio/public/js/yelp.js
+++ Roadio/public/js/yelp.js
@@ -26,7 +26,7 @@ var auth = {
 };
 
 var POICategories =
-    'amusementparks,climbing,beaches,hiking,sailing,surfing,arts,boatcharters,caricatures,facepainting,magicians,musicians,landmarks,resorts,skiresorts,nightlife,localflavor';
+    'amusementparks,climbing,beaches,hiking,sailing,surfing,arts,boatcharters,caricatures,facepainting,magicians,musicians,landmarks,resorts,skiresorts,localflavor';
 
 /* Applies a callback function on a list containing either
 restaurants or points of interest within 25 miles of each location in the LISTOFCOORDS.
